42480255b38bb2f1e2e02b93a1d6572bf5ba8afa
Brett Weaver <brett@weav.net>
2012-12-05 11:23:30 -0800

    adding ruby version check to setup.sh

--- setup.sh
+++ setup.sh
@@ -14,8 +14,24 @@ if [ ! `which git` ]; then
   exit 1
 fi
 
+if [ ! `which ruby` ]; then
+  echo "Ruby must be installed."
+  exit 1
+fi
+
+RUBY_VERSION=`ruby -v | grep ruby | awk {'print $2'} |cut -b1-3`
+if [ "$RUBY_VERSION" != "1.9" ]; then
+  echo "Error: Ruby version must be 1.9.X"
+  exit 1
+fi
+
 if [ ! `which heirloom` ]; then
   echo "Error: Heirloom must be installed."
+  exit 1
+fi
+
+if [ ! `which simple_deploy` ]; then
+  echo "Error: Simple Deploy must be installed."
   exit 1
 fi
 
