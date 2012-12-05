b96a46c591c4b78c97dd553dcde8d51775d9b5d7
Brett Weaver <brett@weav.net>
2012-12-05 11:34:11 -0800

    adding check to setup

--- setup.sh
+++ setup.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-USER=`whoami`
 DIR=`pwd`
 DATABAG_PASSWORD=password
 
-if [ $USER == "root" ]; then
-  echo "Error: Cannot be run as root."
-  exit 1
-fi
-
-if [ ! `which git` ]; then
-  echo "Error: Git must be installed."
-  exit 1
-fi
-
-if [ ! `which ruby` ]; then
-  echo "Ruby must be installed."
-  exit 1
-fi
-
-RUBY_VERSION=`ruby -v | grep ruby | awk {'print $2'} |cut -b1-3`
-if [ "$RUBY_VERSION" != "1.9" ]; then
-  echo "Error: Ruby version must be 1.9.X"
-  exit 1
-fi
-
-if [ ! `which heirloom` ]; then
-  echo "Error: Heirloom must be installed."
-  exit 1
-fi
-
-if [ ! `which simple_deploy` ]; then
-  echo "Error: Simple Deploy must be installed."
-  exit 1
-fi
+. ./check.sh
 
 # Read app name
 read -p "Your application name (only letters, numbers and dashes): " APP_PREFIX
