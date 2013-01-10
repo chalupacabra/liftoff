c25f07c5218eaa8a8f4a1229008c8c233d8ea30d
Brett Weaver <brett@weav.net>
2013-01-10 09:22:39 -0800

    moving cookbooks to berksfile

--- chef-repo/roles/example.rb
+++ chef-repo/roles/example.rb
@@ -3,3 +3,9 @@ description 'Example deploy recipe for use w/ simple deploy'
 run_list 'role[base]', 'recipe[build-essential]', 
          'recipe[git]', 'recipe[apache2]', 
          'recipe[intu_simple_deploy_example]'
+
+override_attributes(
+  'apache' => {
+    'default_site_enabled' => true
+  }
+)
